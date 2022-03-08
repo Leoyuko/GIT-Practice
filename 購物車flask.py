dc04271924f12bebe2e1c42db0e301c01e7ca296
luckeyforme <81143076+luckeyforme@users.noreply.github.com>
2022-03-08 14:53:25 +0800

    Add files via upload

---
+++ 購物車flask.py
@@ -0,0 +1,17 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return"""<h1>購物車shopping<h1> 
+<p>商品清單</p >
+<p>Smart Watch</p > 
+<p>Wallet</p >
+<p>Headphone</p >
+<p>Digital Camera</p >
+"""
+
+if __name__=="__main__":
+    app.run()
+
