75ac3d27e21fb41c57c52fe2087e2f46113f30cd
luckeyforme <81143076+luckeyforme@users.noreply.github.com>
2022-03-08 15:08:35 +0800

    Update 購物車flask.py

--- 購物車flask.py
+++ 購物車flask.py
@@ -1,18 +1,8 @@
-pip3 install flask
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route("/")
-def home():
-    return"""<h1>購物車shopping<h1> 
+<h1>購物車shopping<h1> 
 <p>商品清單</p >
 <p>Smart Watch</p > 
 <p>Wallet</p >
 <p>Headphone</p >
 <p>Digital Camera</p >
-"""
-
-if __name__=="__main__":
-    app.run()
+<button>會員登入</button>
 
